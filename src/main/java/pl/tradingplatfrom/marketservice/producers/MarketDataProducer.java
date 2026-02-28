4d20c48339931fe65c9c562edc239d6f0a43ab71
qbox <jakubprzydatek99@gmail.com>
2026-03-01 00:33:16 +0100

    server port added and unused imports removed

--- src/main/java/pl/tradingplatfrom/marketservice/producers/MarketDataProducer.java
+++ src/main/java/pl/tradingplatfrom/marketservice/producers/MarketDataProducer.java
@@ -2,7 +2,6 @@ package pl.tradingplatfrom.marketservice.producers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,9 +9,6 @@ import pl.tradingplatform.events.StockPriceEvent;
 import pl.tradingplatfrom.marketservice.configuration.MarketConfig;
 import pl.tradingplatfrom.marketservice.data_generator.RandomStockPriceEventGenerator;
 import pl.tradingplatfrom.marketservice.services.StockPriceService;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
